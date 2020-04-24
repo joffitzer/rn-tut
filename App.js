dddb1bdef2758a6339b4559767ce68ca47a2e943
joffitzer <joffitzer@gmail.com>
2020-04-24 11:40:03 -0400

    useState hook in react native, with button to update state

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('jonah');
+  const [person, setPerson] = useState({ name: 'mario', age: 40})
+
+  const clickHandler = () => {
+    setName('new name');
+    setPerson({name: 'luigi', age: 45})
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, world!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Lorem ipsum</Text>
-        <Text>Lorem ipsum</Text>
-        <Text>Lorem ipsum</Text>
-      </View>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}></View>
+        <Button title='update state' onPress={clickHandler}/>
     </View>
   );
 }
@@ -23,15 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20
   }
 });
